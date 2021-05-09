020abdb22cbdba49ee479946c6e55d78f632370c
unknown <josemauricio.gomes@gmail.com>
2021-05-09 17:10:29 -0300

    inicio do projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4b58ba6bf96fcacd1350bc127ca108ed1cd0eb97',
+    'name' => 'hcodebr/ecommerce',
+  ),
+  'versions' => 
+  array (
+    'hcodebr/ecommerce' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b58ba6bf96fcacd1350bc127ca108ed1cd0eb97',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v5.2.22',
+      'version' => '5.2.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+  ),
+);
